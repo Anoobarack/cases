826663de3682b58630c67618eb455a203524ac72
Anoobarack <asimoxxx@gmail.com>
2017-04-29 02:20:36 +0700

    Update case6.py

--- case6.py
+++ case6.py
@@ -17,11 +17,11 @@ def main():
     LETTERSSMALL = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyz'
     LETTERS = LETTERSBIG + LETTERSSMALL
     NUMBERS = '0123456789'
-    SIGNS = '.,!? '
+    SIGNS = '-.,!? '
     ALLOWED_SYMBS = LETTERS + NUMBERS + SIGNS
 
     # Отбираем текст из файла.  
-    f = open(filename, 'r', encoding = 'utf8')
+    f = open(filename, 'r', encoding = 'cp1251')
     text = ''
     for line in f:
         for symbol in line:
@@ -41,20 +41,31 @@ def main():
     List_of_words = text.split(' ')
     if '' in List_of_words:
         List_of_words.remove('')
-    List_of_capitals = []    # Список заглавных слов  
+    List_of_capitals = []    # Список заглавных слов
+    List_of_endings = []    # Список конечных слов  
     for element in List_of_words:
+        if element == '':
+            continue
         if element[0] in LETTERSBIG:
             List_of_capitals.append(element)
+        if element[len(element)-1] in '.?!':
+            List_of_endings.append(element)
     Num_of_capitals = len(List_of_capitals)
     d = {}
     for i in range(len(List_of_words) - 1):
         if List_of_words[i] != '' and List_of_words[i+1] != '':
             if d.get(List_of_words[i]):
-                if List_of_words[i+1] != d[List_of_words[i]]:
+                if type(d.get(List_of_words[i])) == list:
                     a = []
-                    a.append(d[List_of_words[i]])
                     a.append(List_of_words[i+1])
+                    a.extend(d.get(List_of_words[i]))
                     d[List_of_words[i]] = a
+                else:
+                    if List_of_words[i+1] != d[List_of_words[i]]:
+                        a = []
+                        a.append(d[List_of_words[i]])
+                        a.append(List_of_words[i+1])
+                        d[List_of_words[i]] = a
             else:
                 d[List_of_words[i]] = List_of_words[i+1]
     
@@ -64,8 +75,9 @@ def main():
         random.seed()
         lastword = List_of_capitals[random.randint(0, Num_of_capitals - 1)]
         iterations = random.randint(4, 19)
+        ended = False
         for j in range(iterations):
-            if lastword[len(lastword)-1] != '.':
+            if lastword not in List_of_endings:
                 string += ' '
                 if type(d[lastword]) == list:
                     links = len(d[lastword])
@@ -73,17 +85,20 @@ def main():
                     links = 1
                 if links == 1:
                     word = d[lastword]
-                    if word[len(word)-1] == '.':
+                    if word in List_of_endings:
                         j = iterations
                 else:
                     word = d[lastword][random.randint(0,links-1)]
-                    if word[len(word)-1] == '.':
+                    if word in List_of_endings:
                         j = iterations
                 string += lastword
                 lastword = word
-            elif lastword[len(lastword)-1] == '.':
+            elif lastword in List_of_endings:
                 string += ' ' + lastword
-                j = iterations
+                ended = True
+                break
+        if ended == False:
+            string+=' '+List_of_endings[random.randint(0,len(List_of_endings)-1)]
     print(string, end = '')
 
 main()
