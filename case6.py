96196b664591e8b454581a1fa04f71470ba54034
Anoobarack <asimoxxx@gmail.com>
2017-04-28 21:10:34 +0700

    Update case6.py

--- case6.py
+++ case6.py
@@ -13,9 +13,9 @@ def main():
     amount_of_senstences = int(input('Количество генерируемых предложений: '))
 
     # Создаём строку разрешённых символов.  
-    LETTERS1 = 'аАбБвВгГдДеЕёЁжЖзЗиИйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩъыьэЭюЮяЯ'
-    LETTERS2 = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ'
-    LETTERS = LETTERS1 + LETTERS2
+    LETTERSBIG = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    LETTERSSMALL = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyz'
+    LETTERS = LETTERSBIG + LETTERSSMALL
     NUMBERS = '0123456789'
     SIGNS = '.,!? '
     ALLOWED_SYMBS = LETTERS + NUMBERS + SIGNS
@@ -39,9 +39,11 @@ def main():
 
     # Установление связей.  
     List_of_words = text.split(' ')
+    if '' in List_of_words:
+        List_of_words.remove('')
     List_of_capitals = []    # Список заглавных слов  
     for element in List_of_words:
-        if element == element.capitalize():
+        if element[0] in LETTERSBIG:
             List_of_capitals.append(element)
     Num_of_capitals = len(List_of_capitals)
     d = {}
@@ -62,21 +64,26 @@ def main():
         random.seed()
         lastword = List_of_capitals[random.randint(0, Num_of_capitals - 1)]
         iterations = random.randint(4, 19)
-        string += lastword
         for j in range(iterations):
-            string += ' '
-            links = len(d[lastword])
-            if links == 1:
-                word = d[lastword]
-                if word[len(word)-1] == '.':
-                    j = iterations
-            else:
-                word = d[lastword][random.randint(0,links-1)]
-                if word[len(word)-1] == '.':
-                    j = iterations
-            string += lastword
-            lastword = word
+            if lastword[len(lastword)-1] != '.':
+                string += ' '
+                if type(d[lastword]) == list:
+                    links = len(d[lastword])
+                else:
+                    links = 1
+                if links == 1:
+                    word = d[lastword]
+                    if word[len(word)-1] == '.':
+                        j = iterations
+                else:
+                    word = d[lastword][random.randint(0,links-1)]
+                    if word[len(word)-1] == '.':
+                        j = iterations
+                string += lastword
+                lastword = word
+            elif lastword[len(lastword)-1] == '.':
+                string += ' ' + lastword
+                j = iterations
     print(string, end = '')
-        
 
 main()
